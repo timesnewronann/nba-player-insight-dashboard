45b85cfadd537c6a6ba4f27d8ffa95be3e67f5e2
timesnewronann <ronanborja26@gmail.com>
2026-04-03 22:02:58 -0700

    Added notes into learning log and created the data table

--- LEARNINGLOG.md
+++ LEARNINGLOG.md
@@ -12,3 +12,44 @@
     - pandas -> Inspect and shape data
     - psycopg2-binary -> Connect Python to Postgres
 - Created first schema draft
+
+### April 3rd 2026
+
+- Turned the schema draft into SQL
+- Loaded the schema into Postgres
+- DDL = Data Definition Language
+- Defining Structure:
+  - CREATE TABLE
+  - ALTER TABLE
+  - DROP TABLE
+- Different from Data Manipulation:
+  - INSERT
+  - UPDATE
+  - SELECT
+  - DELETE
+- Primary key uniquely identifies one row in a table
+- Example:
+  - One specific player row
+  - One specific team row
+- Foreign key creates a relationship between tables
+  - players.team_id -> teams_id
+  - player_season_stats.player_id -> players.id
+- Constraints are rules the database enforces
+  - NOT NULL
+  - UNIQUE
+  - PRIMARY KEY
+  - REFERNECES
+
+# DATA TABLE
+
+```
+nba_player_insight=# \dt
+                  List of relations
+ Schema |        Name         | Type  |     Owner
+--------+---------------------+-------+---------------
+ public | player_season_stats | table | timesnewronan
+ public | players             | table | timesnewronan
+ public | teams               | table | timesnewronan
+(3 rows)
+```
+Use \q to quit out of psql
--- sql/001_init_schema.sql
+++ sql/001_init_schema.sql
@@ -1,10 +1,42 @@
-CREATE TABLE teams {
-    id BIGSERIAL primary key
+CREATE TABLE teams (
+    id BIGSERIAL PRIMARY KEY,
     nba_team_id INTEGER NOT NULL UNIQUE,
-    team_name VARCHAR(100)NOT NULL,
+    team_name VARCHAR(100) NOT NULL,
     abbreviation VARCHAR(10) NOT NULL,
     city VARCHAR(100),
     conference VARCHAR(20),
     division VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-};
+);
+
+CREATE TABLE players (
+    id BIGSERIAL PRIMARY KEY,
+    nba_player_id INTEGER NOT NULL UNIQUE,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    full_name VARCHAR(200) NOT NULL,
+    team_id BIGINT REFERENCES teams(id),
+    position VARCHAR(20),
+    height VARCHAR(20),
+    weight VARCHAR(20),
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE player_season_stats (
+    id BIGSERIAL PRIMARY KEY,
+    player_id BIGINT NOT NULL REFERENCES players(id),
+    season VARCHAR(20) NOT NULL,
+    games_played INTEGER,
+    minutes_per_game NUMERIC(5,2),
+    points_per_game NUMERIC(5,2),
+    rebounds_per_game NUMERIC(5,2),
+    assists_per_game NUMERIC(5,2),
+    steals_per_game NUMERIC(5,2),
+    blocks_per_game NUMERIC(5,2),
+    field_goal_pct NUMERIC(5,3),
+    three_point_pct NUMERIC(5,3),
+    free_throw_pct NUMERIC(5,3),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (player_id, season)
+);
