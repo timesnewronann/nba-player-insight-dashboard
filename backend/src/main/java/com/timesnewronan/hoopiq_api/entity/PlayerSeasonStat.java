b4887cdf6c2046d92061da9a18f679c0f46fb59d
timesnewronann <ronanborja26@gmail.com>
2026-04-08 18:23:56 -0700

    Adding the player season stats getters and setters

--- backend/src/main/java/com/timesnewronan/hoopiq_api/entity/PlayerSeasonStat.java
+++ backend/src/main/java/com/timesnewronan/hoopiq_api/entity/PlayerSeasonStat.java
@@ -33,5 +33,76 @@ public class PlayerSeasonStat {
     private String season;
 
     // player season stats variables
+    private Integer gamesPlayed;
+    private BigDecimal minutesPerGame;
+    private BigDecimal pointsPerGame;
+    private BigDecimal reboundsPerGame;
+    private BigDecimal assistsPerGame;
+    private BigDecimal stealsPerGame;
+    private BigDecimal blocksPerGame;
+    private BigDecimal fieldGoalPct;
+    private BigDecimal threePointPct;
+    private BigDecimal freeThrowPct;
+
+    public PlayerSeasonStat() {
+
+    }
+
+    // getter to get Id
+    public Long getId() {
+        return id;
+    }
+
+    // getter to get the player
+    public Player getPlayer() {
+        return player;
+    }
+
+    // setter to set the player
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    // getter to get the season
+    public String getSeason() {
+        return season;
+    }
+
+    // setter to set the season
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    // getter to get the gamesPlayed
+    public Integer getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    // setter to set games played
+    public void setGamesPlayed(Integer gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    // getter to get minutes per game
+    public BigDecimal getMinutesPerGame() {
+        return minutesPerGame;
+    }
+
+    // setter to set minutes per game
+    public void setMinutesPerGame(BigDecimal minutesPerGame) {
+        this.minutesPerGame = minutesPerGame;
+    }
+
+    // getter to get the points per game
+    public BigDecimal getPointsPerGame() {
+        return pointsPerGame;
+    }
+
+    // setter to set the points per game
+    public void setPointsPerGame(BigDecimal pointsPerGame) {
+        this.pointsPerGame = pointsPerGame;
+    }
+
+    // getter to get the rebounds per game
 
 }
