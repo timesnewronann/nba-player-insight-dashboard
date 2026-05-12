38e71f92e85f285c34f4c62ab106e1e76a63eceb
timesnewronann <ronanborja26@gmail.com>
2026-05-11 21:41:42 -0700

    Completed async functions for playerService.ts

--- frontend/src/services/PlayerService.ts
+++ frontend/src/services/PlayerService.ts
@@ -1,25 +1,30 @@
 import axios from "axios";
 import type { Player } from "../types/Player";
 import type { SeasonStat } from "../types/SeasonStat";
+import type { GameLog } from "../types/GameLog";
 
-async function searchPlayers(query: string): Promise<Player[]> {
+export async function searchPlayers(query: string): Promise<Player[]> {
   const response = await axios.get(
     `http://localhost:8080/api/players/search?query=${query}`,
   );
   return response.data;
 }
-export default searchPlayers;
 
-async function getPlayerById(id: number): Promise<Player[]> {
-  const response = await axios.get(`http://localhost:8000/api/players/${id}`);
+export async function getPlayerById(id: number): Promise<Player> {
+  const response = await axios.get(`http://localhost:8080/api/players/${id}`);
   return response.data;
 }
 
-async function getSeasonStats(id: number): Promise<SeasonStat> {
+export async function getSeasonStats(id: number): Promise<SeasonStat[]> {
   const response = await axios.get(
-    `http://localhost:8000/api/players/${id}/season-stats`,
+    `http://localhost:8080/api/players/${id}/season-stats`,
   );
   return response.data;
 }
 
-async function getGameLogs(id: number): 
+export async function getGameLogs(id: number): Promise<GameLog[]> {
+  const response = await axios.get(
+    `http://localhost:8080/api/players/${id}/games`,
+  );
+  return response.data;
+}
