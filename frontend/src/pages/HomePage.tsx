aa9fe205ae14d0838199d40dc2979909973cdd8d
timesnewronann <ronanborja26@gmail.com>
2026-05-13 21:26:34 -0700

    Updated home page, search styling, navbar needs to be clickable and hover on search isn't working properly

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -16,22 +16,31 @@ function HomePage() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1>READ THE FLOOR</h1>
-            <p>Shot charts. Game logs. Season Stats. Every player, Every Game. All in One Place</p>
-            <input type="text" 
-            placeholder="Search any NBA player..."
-            value={searchQuery}
-            onChange={(e) => {
-                setSearchQuery(e.target.value)
-                handleSearch()
-            }}
-            />
-            <button onClick={handleSearch}>Search</button>
-            {players.map((player) => (
-                <Link to={`/player/${player.id}`} key={player.id}>
-                    {player.fullName}
-                </Link>
-            ))}
+            <h1 className="font-display text-8xl text-fg-text tracking-wide text-center">READ THE FLOOR</h1>
+            <p className="text-fg-muted text-center max-w-md mt-4">Shot charts. Game logs. Season Stats. Every player, Every Game. All in One Place</p>
+            <div className="relative w-full max-w-md mt-8">
+                <input className="w-full max-w-md bg-fg-bg3 border border-white/10 rounded px-4 py-3 text-fg-text placeholder-fg-muted outline-none focus:border-fg-accent mt-8" type="text" 
+                placeholder="Search any NBA player..."
+                value={searchQuery}
+                onChange={(e) => {
+                    setSearchQuery(e.target.value)
+                    handleSearch()
+                }}
+                />
+                <button className="mt-3 px-8 bg-fg-aceent text-white font-semibold rounded hover:bg-orange-600 transition-colors" onClick={handleSearch}>Search</button>
+            </div>
+            {players.length > 0 && (
+                <div className="w-full max-w-md bg-fg-bg3 border border-white/10 rounded mt-1">
+                    {players.map((player) => (
+                        <Link to={`/player/${player.id}`}
+                         key={player.id}
+                         className="block px-4 py-3 text-fg-text hover:bg-fg-accent/10 hover:text-fg-accent border-b border-white/5 last:border-0 transition-colors"
+                        >
+                            {player.fullName}
+                        </Link>
+                    ))}
+                </div>
+            )}
 
         </div>
         )
