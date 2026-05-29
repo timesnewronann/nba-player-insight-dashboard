cf0a051a3453f85df46c8e42fa04ff2a479cf07a
timesnewronann <ronanborja26@gmail.com>
2026-05-28 22:51:36 -0700

    Finished ShotChartService working on ShotChartController

---
+++ backend/src/main/java/com/timesnewronan/hoopiq_api/controller/ShotChartController.java
@@ -0,0 +1,16 @@
+package com.timesnewronan.hoopiq_api.controller;
+
+import com.timesnewronan.hoopiq_api.entity.Player;
+import com.timesnewronan.hoopiq_api.entity.ShotChart;
+
+import com.timesnewronan.hoopiq_api.service.ShotChartService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class ShotChartController {
+
+}
